507bd954c65590b145274f6dec0214ce2f3b3aee
Lawson Fulton <lawson.fulton@autodesk.com>
2014-12-09 02:11:18 -0800

    Finally done. Just need t clean

--- vendorlist/views.py
+++ vendorlist/views.py
@@ -6,5 +6,5 @@ from vendorlist.models import VendorEvent, Vendor, Location
 
 def index(request):
     vendor_event_counts = Vendor.objects.get_sorted_event_counts(days_ago=30)
-    context = {'vendor_event_counts': vendor_event_counts}
+    context = {'vendor_event_counts': vendor_event_counts, 'days_ago':30}
     return render(request, 'vendorlist/index.html', context)
