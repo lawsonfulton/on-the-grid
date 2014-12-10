90692af22765e854daf9800d88e878758d35e716
Lawson Fulton <lawson.fulton@autodesk.com>
2014-12-09 17:22:17 -0800

    debug on

--- onthegrid/settings.py
+++ onthegrid/settings.py
@@ -16,7 +16,7 @@ import os
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 
 DATABASES = {}
